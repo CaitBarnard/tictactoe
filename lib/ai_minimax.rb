944d052d2a03409f3644d782aa85730db503f1d5
Caitlin Barnard <cait@Caitlins-MacBook-Pro.local>
2019-09-18 12:16:38 +0100

    minimax tree

--- lib/ai_minimax.rb
+++ lib/ai_minimax.rb
@@ -1,3 +1,34 @@
 class Minimax
 
+
+    def self.best_move(available_cells)
+       
+        if available_cells[0].empty?
+            return nil
+        end
+        
+        available_cells.each do |cell|
+            if cell[:children].empty? == false 
+
+                cell[:children].each do |childrens|
+                    if childrens[:children].empty? == false
+                        best_score = childrens[:children].max_by {|childs|childs[:score]}[:score]
+                        best_score -= 1
+                        childrens[:score] = best_score
+
+
+                        puts childrens[:score][]
+                    end
+                end
+
+                best_score = cell[:children].max_by {|childs| childs[:score]}[:score]
+                best_score -= 1
+                cell[:score] += best_score
+
+
+                
+            end
+        end
+        available_cells.max_by {|cells| cells[:score]}[:position]
+    end
 end
--- spec/unit/ai_minimax_spec.rb
+++ spec/unit/ai_minimax_spec.rb
@@ -2,7 +2,69 @@ require 'ai_minimax'
 
 describe Minimax do 
 
-    it "" do
+    it "returns nil when there are no more moves available" do
+        available_cells = [{}]
+        expect(Minimax.best_move(available_cells)).to eq(nil)
     end
     
+    it "returns only position when one available" do
+        available_cells = [{position: 1, children: [], score: 0}]
+        expect(Minimax.best_move(available_cells)).to eq(1)
+    end
+    
+    it "returns the position with the winning score" do 
+        available_cells = [{position: 1, children: [], score: 0},
+                           {position: 4, children: [], score: 10}]
+        expect(Minimax.best_move(available_cells)).to eq(4)
+    end
+
+    it "returns the score of the highest child" do
+        available_cells = [{position: 1, children: [], score: 0},
+                           {position: 4, children: [
+                               {position: 2, children: [], score: 10}
+                           ], score: 0}]
+        expect(Minimax.best_move(available_cells)).to eq(4)
+    end
+
+    it "returns the score of the highest child when multiple children" do
+        available_cells = [{position: 1, children: [
+            {position: 3, children: [], score: 10}
+        ], score: 0},
+        {position: 4, children: [
+            {position: 2, children: [], score: 0}
+        ], score: 0}]
+        expect(Minimax.best_move(available_cells)).to eq(1)
+    end
+
+    it "returns the score of the quickest highest child" do
+        available_cells = [{position: 4, children: [
+            {position: 2, children: [], score: 10},
+            {position: 3, children: [], score: 0}
+        ], score: 0},
+            {position: 1, children: [], score: 10}]
+        expect(Minimax.best_move(available_cells)).to eq(1)
+    end
+
+    it "returns the score of the child's child" do 
+        available_cells = [{position: 4, children: [
+            {position: 2, children: [
+                {position: 5, children: [], score: 10}
+            ], score: 0},
+            {position: 3, children: [], score: 0}
+        ], score: 0},
+    {position: 1, children: [], score: 0}]
+        expect(Minimax.best_move(available_cells)).to eq(4)
+    end
+
+    it "returns draw when there are only losses" do
+        available_cells = [{position: 4, children: [
+            {position: 2, children: [], score: 0},
+            {position: 3, children: [], score: -10}
+        ], score: 0},
+        {position: 1, children: [
+            {position: 5, children: [], score: 0}
+        ], score: 0}]
+        expect(Minimax.best_move(available_cells)).to eq(1)
+    end
+
 end
