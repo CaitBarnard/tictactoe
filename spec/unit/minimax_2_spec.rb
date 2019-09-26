9451bb8595cd3060b4ea8bfab87dc451e4f3be91
Caitlin Barnard <cait@Caitlins-MacBook-Pro.local>
2019-09-26 09:33:48 +0100

    removed incorrect tests in minimax

--- spec/unit/minimax_2_spec.rb
+++ spec/unit/minimax_2_spec.rb
@@ -116,20 +116,6 @@ describe MinMax do
     end
   
     it "returns best position with depth of 2" do
-      tree = [{position: 2, children: [{position: 3, children: [], score: 0}, {position: 5, children: [{position: 9, children: [], score: 10}], score: 0}], score: 0},
-              {position: 3, children: [], score: 0}]
-      expect(MinMax.best_move(tree)).to eq(2)
-    end
-  
-    it "returns best position with depth of 2" do
-      tree = [{position: 2, children: [{position: 3, children: [], score: 0},
-                                       {position: 5, children: [{position: 9, children: [], score: 10},
-                                                                {position: 8, children: [], score: 0}], score: 0}], score: 0},
-              {position: 3, children: [], score: 0}]
-      expect(MinMax.best_move(tree)).to eq(2)
-    end
-  
-    it "returns best position with depth of 2" do
       tree = [{position: 2, children: [{position: 3, children: [], score: -10},
                                        {position: 5, children: [{position: 9, children: [], score: 10},
                                                                 {position: 8, children: [], score: 0}], score: 0}], score: 0},
